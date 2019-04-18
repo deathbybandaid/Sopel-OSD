8a2e3c3c1723ab49f016c1b6bc7319bf900b455a
deathbybandaid <sam@deathbybandaid.net>
2019-04-18 17:37:12 -0400

    test

--- sopel_modules/osd/__init__.py
+++ sopel_modules/osd/__init__.py
@@ -64,8 +64,8 @@ def parse_event_005(bot, trigger):
         if '=' in param:
             if param.startswith("TARGMAX"):
                 param = str(param).split('=')[1]
-                stderr(str(param) + " test " + str(param))
                 settings = str(param).split(',')
+                stderr(str(settings))
                 for setting in settings:
                     setting = str(settings).split(':')[0]
                     value = str(settings).split(':')[1] or None
