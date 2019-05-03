06473961885b0751283ddc672bdbf20b85ad8361
deathbybandaid <sam@deathbybandaid.net>
2019-05-03 12:22:19 -0400

    Handling for stock sopel

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('dev-requirements.txt') as dev_requirements_file:
 
 setup(
     name='sopel_modules.osd',
-    version='0.1.0',
+    version='0.1.1',
     description='Sopel OSD is a "niche" method of displaying text in a Sopel bot',
     long_description=readme + '\n\n' + history,
     author='Sam Zick',
